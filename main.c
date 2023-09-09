10221de935d162be5e8fa51cfabdc839d99b0329
Shintaro Onogi <shinyonogi@outlook.de>
2023-09-10 01:28:59 +0200

    ADDED gets standard input by the user

--- main.c
+++ main.c
@@ -6,6 +6,22 @@
 #include <sys/wait.h>
 
 int main() {
+
+    char input[512];
+    char *args[10];
+
+    while (1) {
+        printf("myshell> ");
+        fgets(input, 512, stdin);
+
+        size_t len = strlen(input);
+        if (len > 0 && input[len-1] == '\n') {
+            input[len-1] = '\0';
+        }
+
+        printf("The command: %s\n", input);
+    }
+
     return 0;
 }
 
